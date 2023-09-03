64bcd7d9439ca47b695c5a76ec54e8947a82ce52
Azha Balouch <azha.nawaz@belouch.com>
2023-09-04 01:54:38 +0500

    Update Header.js
    Added OGP tags for sharing web pages on social media. Also, Improving website’s search engine optimization using this code.

--- little-lemon/src/Header.js
+++ little-lemon/src/Header.js
@@ -19,6 +19,13 @@ function Header() {
         <meta name="rating" content="safe for kids" />
         <meta name="copyright" content="Copyright 2023 Little Lemon Restaurant" />
 
+        {/* Open Graph Protocol (OGP) tags for social media sharing */}
+        <meta property="og:title" content="Little Lemon Restaurant" />
+        <meta property="og:description" content="Explore Little Lemon Restaurant in Chicago - A charming neighborhood bistro offering simple food and classic cocktails in a lively, casual atmosphere with locally-sourced daily specials." />
+        <meta property="og:image" content={Images.logoImage} />
+        <meta property="og:url" content="https://example.com/little-lemon" />
+        <meta property="og:type" content="website" />
+
         {/* HTTP-equiv meta tags */}
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <meta http-equiv="default-style" content="Your default style" />
